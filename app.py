05c6957763947717403fe86ae1146ffcc7b87ae1
mbouanik <mbouanik@student.42.fr>
2023-08-19 15:28:22 -0500

    add css

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, flash, redirect, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import surveys
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+debug = DebugToolbarExtension(app)
+responses = []
+
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html", surveys=surveys["satisfaction"], responses=responses
+    )
+
+
+@app.route("/questions/<int:q>", methods=["GET"])
+def questions(q):
+    if len(surveys["satisfaction"].questions) == len(responses):
+        return redirect("/thankyou")
+    if q != len(responses):
+        flash("Denied invalid question sequences", "error")
+        return redirect(f"/questions/{len(responses)}")
+    question = surveys["satisfaction"].questions[q].question
+    choices = surveys["satisfaction"].questions[q].choices
+    return render_template("question.html", question=question, choices=choices)
+
+
+@app.route("/answer", methods=["GET", "POST"])
+def answer():
+    global responses
+    if len(surveys["satisfaction"].questions) == len(responses):
+        return redirect("/")
+
+    responses += list(request.args)
+
+    return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/thankyou")
+def thanks():
+    return render_template("thankyou.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,21 @@
+const flash = $(".error");
+
+setTimeout(() => {
+  flash.hide(500);
+}, 4000);
+
+const checkbox = $(".checkbox");
+const choices = $(".choices");
+
+choices.on("click", (evt) => {
+  checkbox.prop("checked", false);
+  choices.removeClass("select");
+
+  if (evt.target.tagName === "LABEL") {
+    evt.target.nextElementSibling.checked = true;
+    $(evt.target.parentElement).addClass("select");
+  } else {
+    evt.target.children[1].checked = true;
+    $(evt.target).addClass("select");
+  }
+});
