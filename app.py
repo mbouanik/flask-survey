323ae294c7e15318042e1c1f7035b7e72806f0af
mbouanik <mbouanik@student.42.fr>
2023-09-02 13:38:54 -0500

    session added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, jsonify
+from flask import Flask, render_template, request, flash, redirect, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 
@@ -6,30 +6,37 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
-responses = []
 
 
 @app.route("/")
 def home():
     return render_template(
-        "index.html", surveys=surveys["satisfaction"], responses=responses
+        "index.html",
+        surveys=surveys["satisfaction"],
+        responses=session.get("responses", []),
     )
+
+
+@app.route("/start-survey")
+def start_survey():
+    session["responses"] = []
+    return redirect("questions/0")
 
 
 @app.route("/reset")
 def reset():
-    global responses
-    responses = []
+    session["responses"] = []
     return redirect("/")
 
 
 @app.route("/questions/<int:q>", methods=["GET"])
 def questions(q):
-    if len(surveys["satisfaction"].questions) == len(responses):
+    print(f' LENGTH: {len(list(session["responses"]))} == ={session["responses"]}')
+    if len(surveys["satisfaction"].questions) == len(session["responses"]):
         return redirect("/thankyou")
-    if q != len(responses):
+    if q != len(session["responses"]):
         flash("Denied invalid question sequences", "error")
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(session['responses'])}")
     question = surveys["satisfaction"].questions[q].question
     choices = surveys["satisfaction"].questions[q].choices
     return render_template("question.html", question=question, choices=choices)
@@ -37,15 +44,14 @@ def questions(q):
 
 @app.route("/answer", methods=["GET", "POST"])
 def answer():
-    global responses
     if not request.args:
         flash("Please Select one Answer", "error")
-        return redirect(f"/questions/{len(responses)}")
-    if len(surveys["satisfaction"].questions) == len(responses):
+        return redirect(f"/questions/{len(session['responses'])}")
+    if len(surveys["satisfaction"].questions) == len(session["responses"]):
         return redirect("/")
-    responses += list(request.args)
+    session["responses"] += list(request.args)
 
-    return redirect(f"/questions/{len(responses)}")
+    return redirect(f"/questions/{len(session['responses'])}")
 
 
 @app.route("/thankyou")
